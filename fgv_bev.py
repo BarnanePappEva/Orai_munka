589533d474aebf3d1171cb5eeb8d405e8ca5079b
kevin <hornyak.kevin1@gmail.com>
2021-12-13 02:38:59 -0900

    comment

--- fgv_bev.py
+++ fgv_bev.py
@@ -1,14 +1,14 @@
 def sikerese(pontszam):
       if pontszam > 48:
-            print('Sikeres vizsga!')
+            print('Sikeres vizsga!!!')
       else:
-            print('A vizsga nem sikerült!')
+            print('A vizsga nem sikerült!!!')
 nev=None
 
 while nev !='':
-      nev=input('Kérem a vizsgázó nevét!')
+      nev=input('Kérem a vizsgázó nevét!!!')
       if nev != '':
-            pontszam=input('Kérem a vizsgázó pontszámát!')
+            pontszam=input('Kérem a vizsgázó pontszámát!!!')
             pontszam=int(pontszam)
             if sikerese(pontszam):
                   print(sikerese(pontszam))
--- jelszavas.py
+++ jelszavas.py
@@ -4,7 +4,7 @@ while not beenged:
       nev=input('Add meg a felhasználóneved:')
       jelszo=input('Add meg a jelszavad:')
       if nev=='Bori99' and jelszo=='szivecske':
-            print('Beléphetsz!')
+            print('Beléphetsz!!!')
             beenged=True
       else:
-            print('Belépés megtagadva!')
+            print('Belépés megtagadva!!!')
